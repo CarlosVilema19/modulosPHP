54eb161442737e10df76906c601d1b4cec7df46f
CarlosVilema19 <68031881+CarlosVilema19@users.noreply.github.com>
2022-05-28 11:17:52 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,11 +1,16 @@
 <?php
 $hostname='geolamserver.mysql.database.azure.com';
 $database='geolamdb';
-$username='azure_superuser@geolamadmin';
+$username='geolamadmin';
 $password='Riobamba2';
+
+/*$hostname='localhost';
+$database='geolam';
+$username='root';
+$password='';*/
 
 $conexion=new mysqli($hostname,$username,$password,$database);
 if($conexion->connect_errno){
-    echo "El sitio web está presentando problemas";
+    echo "El sitio web está experimentado problemas";
 }
 ?>
