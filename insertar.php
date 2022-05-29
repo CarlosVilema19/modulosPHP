d8fbf269c9a79d54910ce7bc17ebb59d63c264b1
CarlosVilema19 <68031881+CarlosVilema19@users.noreply.github.com>
2022-05-29 13:08:03 -0500

    Add files via upload

---
+++ insertar.php
@@ -0,0 +1,27 @@
+<?php
+
+$conexion =mysqli_connect('localhost','ftawmurs_geolamadmin','Riobamba22','ftawmurs_geolamdb');
+//$conexion =mysqli_connect('localhost','root','','geolam');
+
+if(!$conexion)
+{
+    echo "error en conexion";
+}
+
+$email =$_POST['email'];
+$nombre_usuario =$_POST['nombre_usuario'];
+$contrasenia =$_POST['contrasenia'];
+
+
+$query = "INSERT INTO usuario (email,id_tipo_usuario,nombre_usuario,contrasenia) values ('$email','1','$nombre_usuario','$contrasenia')";
+//$query = "INSERT INTO usuario (email,id_tipo_usuario,nombre_usuario,contrasenia) values ('pedro@gmail.com','1','pedro','12345')";
+$resultado = mysqli_query($conexion,$query);
+
+if($resultado)
+{
+    echo "registro correcto";
+}else{
+    echo "error";
+}
+
+?>
